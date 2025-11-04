1bd5373e3d2ace5d2fd85c9eed092f293bcde0b6
aldys002 <igidiaaldys@gmail.com>
2025-11-04 09:40:48 +0700

    menambah endpoint delete untuk menghapus film

--- index.js
+++ index.js
@@ -56,3 +56,17 @@ app.put('/film/:id', async (req, res) =>{
         res.status(500).send(err);
     }
 });
+
+app.delete('/film/:id', async (req, res) => {
+    const id = req.params.id;
+    try{
+        const film = await db.Film.findByPk(id);
+        if(!film) {
+            return res.status(404).send({message: 'Film tidak di temukan'});
+        }
+        await film.destroy();
+        res.send({message: 'Film berhasil dihapus'});
+    }catch(err) {
+        res.status(500).send(err);
+    }
+});
