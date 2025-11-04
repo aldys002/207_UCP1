705f7a1ca4d554d4dceec0d22544a2f333396815
aldys002 <igidiaaldys@gmail.com>
2025-11-04 09:39:32 +0700

    menambah endpoint put untuk mengupdate film

--- index.js
+++ index.js
@@ -40,3 +40,19 @@ app.get("/film", async (req, res) => {
         res.send(err);
     }
 });
+
+app.put('/film/:id', async (req, res) =>{
+    const id = req.params.id;
+    const data = req.body;
+
+    try{
+        const film = await db.Film.findByPk(id);
+        if(!film) {
+            return res.status(404).send({message: 'Film tidak di temukan'});
+        }
+        await film.update(data);
+        res.send({message: 'Film berhasil diupdate', film});
+    }catch(err) {
+        res.status(500).send(err);
+    }
+});
